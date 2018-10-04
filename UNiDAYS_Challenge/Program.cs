2324260dc791b9fb64d1475f53c5e5951b74b1eb
Tilman Raendchen <chopper98@hotmail.de>
2018-10-05 00:23:02 +0100

    Create demo and add more comments

--- UNiDAYS_Challenge/Program.cs
+++ UNiDAYS_Challenge/Program.cs
@@ -18,15 +18,45 @@ namespace UNiDAYS_Challenge
         // Create a default rule-table to be used throughout for testing
         public static PricingRules DefaultRules = new PricingRules
             {
+                // No discount
                 new PricingRule(typeof(A)),
+                // Discount for £20 if 2 items exist
                 new PricingRule(typeof(B), 2, 20.0 / 12.0),
+                // Discount for £10 if 3 items exist
                 new PricingRule(typeof(C), 3, 10.0 / 4.0),
+                // Discount for £7 (same price) if 2 items exist
                 new PricingRule(typeof(D), 2, 1),
+                // Discount for £10 (3 for 2) if 3 items exist
                 new PricingRule(typeof(E), 3, 2)
             };
 
+        // This demo represents the example found on the github readme
+        // and shows how this interface can be implemented
+        static void InterfaceDemo()
+        {
+            UnidaysDiscountChallenge challenge = new UnidaysDiscountChallenge(Program.DefaultRules);
+
+            Item itemOne = new A(5);
+            Item itemTwo = new B(12);
+
+            challenge.AddToBasket(itemOne);
+            challenge.AddToBasket(itemTwo);
+
+            PriceResult result = challenge.CalculateTotalPrice();
+
+            double totalPrice = result.Total;
+            double deliveryCharge = result.DeliveryCharge;
+            double overallTotal = totalPrice + deliveryCharge;
+        }
+
         static void Main(string[] args)
         {
+            Console.Write("Welcome to this solution (created by Tilman Raendchen for myUNiDAYS coding challenge)." +
+                "\nSimply press [ RETURN ] to start the test cases...");
+            Console.ReadLine();
+
+            // Uncomment the next line to run through the interface demo
+            //Program.InterfaceDemo();
             Program.RunTestCases();
 
             Console.Write("\n\nThis demo is now finished. Press [ RETURN ] to exit...");
